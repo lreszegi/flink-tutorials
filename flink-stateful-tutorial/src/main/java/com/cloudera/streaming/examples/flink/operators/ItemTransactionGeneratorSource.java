f4168729c132c59284d637f06279ebd78cb5e843
Laszlo Reszegi <lreszegi@cloudera.com>
2020-04-28 10:02:50 +0200

    HCUBE-340 use busy wait for nanosecond grade throttling

--- flink-stateful-tutorial/src/main/java/com/cloudera/streaming/examples/flink/operators/ItemTransactionGeneratorSource.java
+++ flink-stateful-tutorial/src/main/java/com/cloudera/streaming/examples/flink/operators/ItemTransactionGeneratorSource.java
@@ -18,11 +18,10 @@
 
 package com.cloudera.streaming.examples.flink.operators;
 
-import org.apache.flink.api.java.utils.ParameterTool;
-import org.apache.flink.streaming.api.functions.source.ParallelSourceFunction;
-
 import com.cloudera.streaming.examples.flink.types.ItemTransaction;
 import org.apache.commons.math3.distribution.ParetoDistribution;
+import org.apache.flink.api.java.utils.ParameterTool;
+import org.apache.flink.streaming.api.functions.source.ParallelSourceFunction;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -34,61 +33,69 @@ import java.util.concurrent.ThreadLocalRandom;
  */
 public class ItemTransactionGeneratorSource implements ParallelSourceFunction<ItemTransaction> {
 
-	public static final String NUM_ITEMS_KEY = "num.items";
-	public static final String SLEEP_MILLIS_KEY = "sleep.millis";
-	public static final String SLEEP_NANOS_KEY = "sleep.nanos";
-	public static final String PARETO_SHAPE_KEY = "pareto.shape";
-	public static final int DEFAULT_NUM_ITEMS = 1_000;
-	public static final int DEFAULT_SHAPE = 15;
-	private static final Logger LOG = LoggerFactory.getLogger(ItemTransactionGeneratorSource.class);
-	private final int numItems;
-	private final long sleepMillis;
-	private final int sleepNanos;
-	private final int shape;
-	private volatile boolean isRunning = true;
+    public static final String NUM_ITEMS_KEY = "num.items";
+    public static final String SLEEP_MILLIS_KEY = "sleep.millis";
+    public static final String SLEEP_NANOS_KEY = "sleep.nanos";
+    public static final String PARETO_SHAPE_KEY = "pareto.shape";
+    public static final int DEFAULT_NUM_ITEMS = 1_000;
+    public static final int DEFAULT_SHAPE = 15;
+    private static final Logger LOG = LoggerFactory.getLogger(ItemTransactionGeneratorSource.class);
+    private final int numItems;
+    private final long sleepMillis;
+    private final int sleepNanos;
+    private final int shape;
+    private volatile boolean isRunning = true;
 
-	public ItemTransactionGeneratorSource(ParameterTool params) {
-		this.numItems = params.getInt(NUM_ITEMS_KEY, DEFAULT_NUM_ITEMS);
-		this.sleepMillis = params.getInt(SLEEP_MILLIS_KEY, 0);
-		this.sleepNanos = params.getInt(SLEEP_NANOS_KEY, 0);
-		this.shape = params.getInt(PARETO_SHAPE_KEY, DEFAULT_SHAPE);
-	}
+    public ItemTransactionGeneratorSource(ParameterTool params) {
+        this.numItems = params.getInt(NUM_ITEMS_KEY, DEFAULT_NUM_ITEMS);
+        this.sleepMillis = params.getInt(SLEEP_MILLIS_KEY, 0);
+        this.sleepNanos = params.getInt(SLEEP_NANOS_KEY, 0);
+        this.shape = params.getInt(PARETO_SHAPE_KEY, DEFAULT_SHAPE);
+    }
 
-	@Override
-	public void run(SourceContext<ItemTransaction> ctx) throws Exception {
-		ThreadLocalRandom rnd = ThreadLocalRandom.current();
-		ParetoDistribution paretoDistribution = new ParetoDistribution(numItems, shape);
+    @Override
+    public void run(SourceContext<ItemTransaction> ctx) throws Exception {
+        ThreadLocalRandom rnd = ThreadLocalRandom.current();
+        ParetoDistribution paretoDistribution = new ParetoDistribution(numItems, shape);
 
-		LOG.info("Starting data generator for {} items and {} sleep millis {} sleep nanos", numItems, sleepMillis, sleepNanos);
+        LOG.info("Starting data generator for {} items and {} sleep millis {} sleep nanos", numItems, sleepMillis, sleepNanos);
 
-		while (isRunning) {
-			long nextItemId;
-			do {
-				nextItemId = sample(paretoDistribution);
-			} while (nextItemId > numItems);
-			String itemId = "item_" + nextItemId;
+        while (isRunning) {
+            long nextItemId;
+            do {
+                nextItemId = sample(paretoDistribution);
+            } while (nextItemId > numItems);
+            String itemId = "item_" + nextItemId;
 
-			int quantity = (int) (Math.round(rnd.nextGaussian() / 2 * 10) * 10) + 5;
-			if (quantity == 0) {
-				continue;
-			}
-			long transactionId = rnd.nextLong(Long.MAX_VALUE);
-			synchronized (ctx.getCheckpointLock()) {
-				ctx.collect(new ItemTransaction(transactionId, System.currentTimeMillis(), itemId, quantity));
-			}
-			if (sleepMillis > 0 || sleepNanos > 0) {
-				Thread.sleep(sleepMillis, sleepNanos);
-			}
-		}
+            int quantity = (int) (Math.round(rnd.nextGaussian() / 2 * 10) * 10) + 5;
+            if (quantity == 0) {
+                continue;
+            }
+            long transactionId = rnd.nextLong(Long.MAX_VALUE);
+            synchronized (ctx.getCheckpointLock()) {
+                ctx.collect(new ItemTransaction(transactionId, System.currentTimeMillis(), itemId, quantity));
+            }
+            if (sleepMillis > 0) {
+                Thread.sleep(sleepMillis);
+            }
+            if (sleepNanos > 0) {
+                final long INTERVAL = sleepNanos;
+                long start = System.nanoTime();
+                long end = 0;
+                do {
+                    end = System.nanoTime();
+                } while (start + INTERVAL >= end);
+            }
+        }
 
-	}
+    }
 
-	private long sample(ParetoDistribution paretoDistribution) {
-		return (Math.round(paretoDistribution.sample() - paretoDistribution.getScale()) + 1);
-	}
+    private long sample(ParetoDistribution paretoDistribution) {
+        return (Math.round(paretoDistribution.sample() - paretoDistribution.getScale()) + 1);
+    }
 
-	@Override
-	public void cancel() {
-		isRunning = false;
-	}
+    @Override
+    public void cancel() {
+        isRunning = false;
+    }
 }
